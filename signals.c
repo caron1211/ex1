c4b713e05a3b8a5472d678c9e5203ac7c54577ef
OdeliaHochman <45036697+OdeliaHochman@users.noreply.github.com>
2019-05-30 16:30:52 +0300

    Update signals.c

--- signals.c
+++ signals.c
@@ -11,11 +11,8 @@
 
 
 
-// void sigCathcher(int sig)
-// {
-//     kill(child_pid,SIGKILL);
-// }
 
+void sigCathcher(int sig);
 
 int main()
 { 
@@ -23,27 +20,40 @@ int main()
     int arrChilds[5];
     int zombie;
 
-            printf("\n");
+    printf("\n");
 
-	for(int i=0;i<5;i++) // loop will run n times (n=5) 
+	for(int i=0;i<5;i++) // 5 pid 
 	{ 
 		if(fork() == 0) 
 		{
 			printf("PID %d ready\n",getpid()); 
-			arrChilds[i]=getpid();
-
 			exit(0); 
 		} 
+		arrChilds[i]=getpid();
 	} 
-	for(int i=0;i<5;i++) // loop will run n times (n=5) 
-	wait(NULL);
+ 	for(int i=0;i<5;i++) // loop will run n times (n=5) 
+ 	wait(NULL);
 	
-	
-	// for(int i =0;i<5;i++)   ////send signal to all the children to kill.-------------to fix!
-	// {
-	   
-	//    signal(SIGALRM,(void (*)(int))kill_child); 
-	// }
-	 
-	
+	for(int i =0;i<5;i++)  
+	{
+	  signal(SIGINT,sigCathcher);
+	  kill(getpid(), SIGINT); 
+	  sleep(1);
+	    
+	}
+
+	for(int i =0;i<5;i++)  
+	{
+	   zombie=arrChilds[i];
+	   printf("Process %d is dead \n",zombie);
+	  
+	    
+	}
 } 
+
+void sigCathcher(int sig)
+{
+  printf("PID %d caught one \n",getpid());
+ // signal();
+}
+
