f5cf9a9df74ba194103c3ae533cb94e9f84a8179
OdeliaHochman <45036697+OdeliaHochman@users.noreply.github.com>
2019-05-30 17:38:02 +0300

    fix

--- signals.c
+++ signals.c
@@ -13,7 +13,7 @@
 void sigCathcher(int sig);
 
  int arrChilds[5];         // holds the pids of the children
- int j;                   // index to cpid
+ int j;                   // index to arrChilds
   
 int main()
 {
@@ -32,7 +32,7 @@ int main()
       	pause(); 			// wait for signal
      	exit(0);  			// end process (become a zombie)
     }
-    else            			// Only father updates the cpid array.
+    else            			// Only father updates the arrChilds array.
       arrChilds[i] = pid;	  
 }
 
@@ -57,5 +57,5 @@ void sigCathcher(int sig)
   signal(SIGINT,sigCathcher);  // re-assign the signal catcher
   printf("PID %d caught one\n", getpid());
   if(j > -1)
-    	kill(arrChilds[j], SIGINT);  // send signal to next child in cpid
+    	kill(arrChilds[j], SIGINT);  // send signal to next child in arrChilds
 }
